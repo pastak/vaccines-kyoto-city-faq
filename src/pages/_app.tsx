3d0cf1ae19181187958c1c73cb0c0e5d4183b0db
pastak <pasta0915@gmail.com>
2021-05-14 23:16:13 +0900

    warning対策

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { AppProps } from 'next/app';
+import Head from 'next/head'
 
 import '../index.css';
 import '../App.css';
 import '../FAQList.css';
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return <>
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="theme-color" content="#000000" />
+      <title>京都市新型コロナワクチン接種ポータルサイト よくあるお問合せについて (勝手版)</title>
+    </Head>
+    <Component {...pageProps} />
+  </>
 };
 export default App;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,8 +12,6 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <meta charSet="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta name="theme-color" content="#000000" />
           <meta
             name="description"
             content="このウェブページは京都市新型コロナワクチン接種ポータルサイトのよくあるお問合せについてのPDFをOCR読み取り(画像の自動読み取り)をした結果を掲載したものです。全ての情報については京都市のウェブサイトを確認するようにしてください。"
@@ -21,7 +19,6 @@ class MyDocument extends Document {
           <meta name="twitter:card" content="summary" />
           <meta name="twitter:title" content="京都市新型コロナワクチン接種ポータルサイト よくあるお問合せについて (勝手版)" />
           <meta name="twitter:description" content="このウェブページは京都市新型コロナワクチン接種ポータルサイトのよくあるお問合せについてのPDFをOCR読み取り(画像の自動読み取り)をした結果を掲載したものです。全ての情報については京都市のウェブサイトを確認するようにしてください。" />
-          <title>京都市新型コロナワクチン接種ポータルサイト よくあるお問合せについて (勝手版)</title>
           <link href="%PUBLIC_URL%" rel="canonical" />
         </Head>
         <body>
