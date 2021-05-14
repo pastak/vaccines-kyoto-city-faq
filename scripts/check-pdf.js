1e519c0c47d2fd325ca5864d81bb1087e819d903
pastak <pasta0915@gmail.com>
2021-05-14 11:47:37 +0900

    PDFへのリンクを1時間に1回見る

--- scripts/check-pdf.js
+++ scripts/check-pdf.js
@@ -5,7 +5,7 @@ import puppeteer from 'puppeteer';
   const page = await browser.newPage();
   await page.goto('https://vaccines-kyoto-city.jp');
   const faqLink = await page.evaluate((selector) => {
-    return document.querySelector<HTMLAnchorElement>(selector)?.href;
+    return document.querySelector(selector)?.href;
   }, 'section#faq .sect-txt .txt-link');
   if (!faqLink) return process.exit(1);
   if (!faqLink.includes('yokuaru030427')) return process.exit(1);
