accb1a15fbb685cab146b1bf322eda9814bec81b
pastak <pastak@users.noreply.github.com>
2021-05-15 02:14:23 +0000

    deploy: 0a4a7984021840efecd2551adc03e3d1aa9c1375

--- _next/static/kB-6aI7_j8F5w6pOjNrSn/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-0aac622ade08c4c58bd6.js"],"/_error":["static/chunks/pages/_error-67097000bd67397dbd39.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
