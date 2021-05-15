5a35317c55d5755fcdc2b9b4ecb2201b8cd523d0
pastak <pasta0915@gmail.com>
2021-05-15 10:51:49 +0900

    parse済みのものをexportする

--- src/components/FAQList.tsx
+++ src/components/FAQList.tsx
@@ -1,13 +1,10 @@
-import { useMemo } from "react"
-import { faqParser } from '../lib/parser';
-import { faqData } from './data';
+import { parsedFaqData } from './data';
 
 export const FAQList: React.VFC = () => {
-  const parsedData = useMemo(() => faqParser(faqData), []);
 
   return <>
     {
-      parsedData.map(([question, answer], i) => {
+      parsedFaqData.map(([question, answer], i) => {
         const index = i + 1;
         const id = `faq-${index}`;
         return <section
